bf1479c93e6987db18f2b8a3b3b6a954073a0282
erunyan-cpu <erunyan@tamu.edu>
2026-02-11 21:41:02 -0600

    Update build_powershell.ps1

--- build_powershell.ps1
+++ build_powershell.ps1
@@ -6,10 +6,11 @@ Remove-Item -Recurse -Force .\dist, .\build, .\app.spec -ErrorAction Ignore
 
 # PyInstaller build
 pyinstaller app.py `
+    --name "stomaScope" `
     --onedir `
     --noconsole `
     --clean `
-    --paths "C:/Users/erunyan/AppData/Local/Programs/Python/Python311" `    # update path if not me on my laptop!
+    --paths "C:/Users/erunyan/AppData/Local/Programs/Python/Python311" `  # update path if not your laptop
     --collect-submodules torch `
     --collect-data torch `
     --collect-data customtkinter `
@@ -22,3 +23,4 @@ pyinstaller app.py `
     --hidden-import=sklearn `
     --hidden-import=matplotlib.backends.backend_agg `
     --hidden-import=matplotlib.backends.backend_svg
+
